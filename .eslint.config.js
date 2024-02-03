15ddbc67bf5bb0beb17b3ae427ad602506d4a30b
asasinmode <skin9470@gmail.com>
2024-02-03 07:30:42 +0100

    feat: update dependencies & eslint config

---
+++ .eslint.config.js
@@ -0,0 +1,32 @@
+import antfu from '@antfu/eslint-config';
+
+export default await antfu({
+	stylistic: {
+		semi: true,
+		indent: 'tab',
+	},
+	rules: {
+		'curly': ['error', 'all'],
+		'ts/consistent-type-definitions': ['error', 'type'],
+		'style/brace-style': ['error', '1tbs'],
+		'style/member-delimiter-style': ['error', {
+			multiline: {
+				delimiter: 'semi',
+				requireLast: true,
+			},
+			singleline: {
+				delimiter: 'semi',
+				requireLast: true,
+			},
+		}],
+		'style/comma-dangle': ['error', {
+			arrays: 'always-multiline',
+			objects: 'always-multiline',
+			imports: 'always-multiline',
+			exports: 'always-multiline',
+			functions: 'never',
+		}],
+	},
+	vue: true,
+	typescript: true,
+});
